450082a307ea3ec25fa133cf09bda7099175bcb9
Thomas <collinskthomas@gmail.com>
2023-02-17 18:46:00 +0000

    Add Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Prints all the numbers of base 16
+ *in lowercase, followed by a new line,
+ *using only the putchar function three times.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (i = 'a'; i < 'g'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
