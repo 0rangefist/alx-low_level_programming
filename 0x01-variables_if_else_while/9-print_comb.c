b0172c5ccf2f9c82a63ad35bd3b95b797bcdc749
Thomas <collinskthomas@gmail.com>
2023-02-17 18:58:15 +0000

    Add Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Prints rints all possible
+ *combinations of single-digit numbers
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
