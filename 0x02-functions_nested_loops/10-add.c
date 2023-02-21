d19b6eb2c26eda4d9e9e458a522b8710083f6ccc
Thomas <collinskthomas@gmail.com>
2023-02-21 19:34:37 +0000

    Add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
