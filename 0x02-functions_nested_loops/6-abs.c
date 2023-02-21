9c370307913f7bf361d21c6d89cb5c64f027170c
Thomas <collinskthomas@gmail.com>
2023-02-21 16:30:56 +0000

    Update #3 for task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - prints the absolute val of a number
  * * @n: character to check
- * Return: int
+ * Return: absolute val of a number
  */
 
 int _abs(int n)
@@ -11,5 +11,5 @@ int _abs(int n)
 	if (n >= 0)
 		return (n);
 	else
-		return (n / -1);
+		return (-n);
 }
