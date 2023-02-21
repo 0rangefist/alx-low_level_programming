11301b502aea914a93688008fc75b8cfc6c2c235
Thomas <collinskthomas@gmail.com>
2023-02-21 16:13:11 +0000

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute val of a number
+ * * @n: character to check
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n / -1);
+	}
+}
