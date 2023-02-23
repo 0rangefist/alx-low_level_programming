2fb102ece2639bc4f448076e597ac3198327cabe
Thomas <collinskthomas@gmail.com>
2023-02-23 15:27:43 +0000

    Update #1 for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,15 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != '2' || c != '4')
+		if (c == '2' || c == '4')
+		{
+			/* don nothing */
+		}
+		else
 		{
 			_putchar(c);
 		}
+
 	}
 
 	_putchar('\n');
