31ce0e9cb9fb021a686b77a99fef699de4d55f85
Thomas <collinskthomas@gmail.com>
2023-02-27 19:18:07 +0000

    Add task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: pointer to a string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int  length;
+	int  i;
+	char tmp;
+
+	i      = 0;
+	length = 0;
+
+	/* Find the length of the string */
+	while (str[i++])
+	{
+		length++;
+	}
+
+	/* print every other character*/
+	for (i = 0; i <= length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
