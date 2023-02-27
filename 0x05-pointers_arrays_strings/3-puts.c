b022d7280f098391b6604631067efa3422d4479b
Thomas <collinskthomas@gmail.com>
2023-02-27 16:38:26 +0000

    Add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ *
+ * @str: pointer to a string
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
