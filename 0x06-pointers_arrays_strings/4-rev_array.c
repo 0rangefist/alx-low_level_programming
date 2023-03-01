0b42df1d50a6038c30a68c7bee93ecaaaba4d8e7
Thomas <collinskthomas@gmail.com>
2023-03-01 22:52:58 +0000

    Update #1 for task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,13 @@ void reverse_array(int *a, int n)
 	int temp;
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	if (a == 0)
+	{
+		/* avoid null pointer dereference*/
+		return;
+	}
+
+	for (i = 0; i < n / 2; i++)
 	{
 		temp         = a[i];
 		a[i]         = a[n - i - 1];
