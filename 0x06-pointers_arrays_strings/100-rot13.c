53f2d9bd8870ce12cf65b57e4772880740f5e886
Thomas <collinskthomas@gmail.com>
2023-03-02 13:45:28 +0000

    Add task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @s: pointer to a string
+ *
+ * Return: modified string
+ */
+char *rot13(char *s)
+{
+	char *s_ptr;
+	char *chars_ptr;
+	char *cipher_ptr;
+
+	char chars[]  = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char cipher[] = "mMnNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlL";
+
+	s_ptr = s;
+	while (*s_ptr != '\0')
+	{
+		chars_ptr  = chars;
+		cipher_ptr = cipher;
+		while (*chars_ptr != '\0' && *s_ptr != *chars_ptr)
+		{
+			chars_ptr++;
+			cipher_ptr++;
+		}
+
+		if (*s_ptr == *chars_ptr)
+		{
+			*s_ptr = *cipher_ptr;
+		}
+
+		s_ptr++;
+	}
+	return (s);
+}
