b778faf245a356635844fddd6f09de2ed7134ccd
Thomas <collinskthomas@gmail.com>
2023-03-02 17:11:14 +0000

    Add task 12 (unimplemented)

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+#include <string.h>
+
+/**
+ * infinite_add - Adds two positive numbers represented as strings
+ *
+ * @n1: First number as a string
+ * @n2: Second number as a string
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
+ *
+ * Return: Pointer to the result, or 0 if the result cannot be stored in the
+ * buffer
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r); /* return pointer to result string */
+}
