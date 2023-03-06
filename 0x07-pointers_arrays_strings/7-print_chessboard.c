b82bbec65e7e0ccefc2b5355d47d0bcc9041618d
Thomas <collinskthomas@gmail.com>
2023-03-06 13:00:05 +0000

    Update #1 for task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,12 +12,18 @@ void print_chessboard(char (*a)[8])
 	int row;
 	int col;
 
+	if (sizeof(*a) != 8 * 8)
+	{
+		printf("Error: input array is not 8x8\n");
+		return;
+	}
+
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
 		{
 			_putchar(a[row][col]);
-        }
-        _putchar('\n');
-    }
+		}
+		_putchar('\n');
+	}
 }
