9f35b77e12dd59bf8c8d767518ba8e70d7e48b1d
Thomas <collinskthomas@gmail.com>
2023-03-08 16:30:23 +0000

    Add task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number whose square root is to be found
+ *
+ * Return: integer square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (_sqrt_recursive_helper(n, 0, n));
+}
+
+/**
+ * _sqrt_recursive_helper - recursive helper function to find the
+ * square root of a number
+ *
+ * @n: number whose square root is to be found
+ * @start: starting point of the search
+ * @end: ending point of the search
+ *
+ * Return: integer square root of n
+ */
+int _sqrt_recursive_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid > n)
+	{
+		return (_sqrt_recursive_helper(n, start, mid - 1));
+	}
+	else
+	{
+		return (_sqrt_recursive_helper(n, mid + 1, end));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,4 +17,8 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+int _sqrt_recursion(int n);
+
+int _sqrt_recursive_helper(int, int, int);
+
 #endif
