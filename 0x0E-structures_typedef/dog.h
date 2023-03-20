62aab465463708b4ef59d2050477f5d3d90f8ceb
Thomas <collinskthomas@gmail.com>
2023-03-20 17:17:46 +0000

    Add header file (dog.h)
    - Adds a header file "dog.h" that defines a new type "struct dog"

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+/**
+ * struct dog - custom dog struct
+ *
+ * @name: string that holds the name of the dog
+ * @age: float that holds the age of the dog
+ * @owner: string that holds the owners name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_STRUCT_H */
