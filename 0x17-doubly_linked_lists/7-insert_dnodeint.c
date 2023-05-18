f9f79fafb0b87fb1dc32f98de8871c6dfa642b39
Thomas <collinskthomas@gmail.com>
2023-05-18 15:57:06 +0000

    Update file (7-insert_dnodeint.c)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * create_node - Creates and initializes a new node
+ *
+ * @n: Data of new node to be added
+ *
+ * Return: Pointer to the new node or NULL on failure
+ */
+dlistint_t *create_node(int n)
+{
+	dlistint_t *new_node = NULL; /* ptr of new node to be created */
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL) /* on alloc fail */
+		return (NULL);
+	new_node->n	   = n;	   /* set data variable of node */
+	new_node->next = NULL; /* set next pointer of node */
+	new_node->prev = NULL; /* set prev pointer of node */
+
+	return (new_node);
+}
+
+/**
  * insert_dnodeint_at_index - Inserts new node at a given position
  *
  * @h: Address of pointer to the start of the list
@@ -16,36 +37,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t	*prev_node		 = NULL; /* ptr of previous node in the list */
 	unsigned int curr_node_index = 0;	 /* index of the current node in list */
 
-	if (h == NULL) /* null ptr */
+	if (h == NULL || (*h == NULL && idx > 0)) /* null ptr/idx out of bounds */
 		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL) /* on alloc fail */
-		return (NULL);
-	new_node->n = n;		   /* set data variable of node */
-	if (*h == NULL && idx > 0) /* idx out of bounds for empty list */
+	new_node = create_node(n); /* create and initialize new node */
+	if (new_node == NULL)	   /* on alloc fail */
 		return (NULL);
 	if (idx == 0)
 	{							   /* for insertion at unique case of index 0 */
 		new_node->next = *h;	   /* new node points to old head */
-		new_node->prev = NULL;	   /* new node has no prev node */
 		if (*h != NULL)			   /* not an empty list */
 			(*h)->prev = new_node; /* old head points back to new node */
 		*h = new_node;			   /* update head pointer to new node */
 		return (new_node);
 	}
 	for (curr_node = *h; curr_node != NULL; curr_node_index++)
-	{ /* traverse through the list */
+	{ /* traverse through the list using a for loop */
 		if (curr_node_index == idx)
 		{ /* check if curr_node_index matches lookup index */
-			new_node->next	= curr_node;
-			new_node->prev	= curr_node->prev;
-			prev_node->next = new_node;
-			curr_node->prev = new_node;
+			new_node->next	= curr_node; /* new node points to current node */
+			new_node->prev	= curr_node->prev; /*new node points back to prev*/
+			prev_node->next = new_node; /* previous node points to new node */
+			curr_node->prev = new_node; /* current node points back to new */
 			return (new_node);
 		}
 		prev_node = curr_node; /* set the curr node as the previous node */
 		curr_node = curr_node->next; /* set the next node as the current node */
 	}
-	free(new_node); /* if the lookup index is out of range */
-	return (NULL);
+	if (curr_node_index == idx) /* check if index is equal to list length */
+	{
+		prev_node->next = new_node;	 /* previous node points to new node */
+		new_node->prev	= prev_node; /* new node points back to previous node */
+		return (new_node);
+	}
+	free(new_node);
+	return (NULL); /* if the lookup index is out of range */
 }
