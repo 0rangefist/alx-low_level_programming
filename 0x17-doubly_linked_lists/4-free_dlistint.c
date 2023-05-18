9f484e0f94f55e3786ec80b32167e9841ba2d789
Thomas <collinskthomas@gmail.com>
2023-05-18 13:24:43 +0000

    Add file (4-free_dlistint.c)

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees the memory allocated to a dlistint_t list
+ *
+ * @head: Pointer to the start of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node_to_free;
+
+	if (head == NULL) /* empty list */
+		return;
+
+	/* traverse through the list & free each node */
+	while (head != NULL)
+	{
+		node_to_free = head;
+		head		 = head->next;
+		free(node_to_free);
+	}
+}
