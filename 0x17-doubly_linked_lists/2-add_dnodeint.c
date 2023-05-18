d502b830113995e9a5a04b9a1b7ca0c07cbe2ed0
Thomas <collinskthomas@gmail.com>
2023-05-18 13:12:07 +0000

    Update file (2-add_dnodeint.c)

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -24,7 +24,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n	   = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
-	*head		   = new_node;
+	if (*head != NULL) /* if not an empty list */
+		(*head)->prev = new_node;
+	*head = new_node;
 
 	return (new_node);
 }
