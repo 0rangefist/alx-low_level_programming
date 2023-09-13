7653939ebab94bd81f914f696895ba1c8bc24f1d
Thomas Collins <collinskthomas@gmail.com>
2023-09-13 17:29:52 +0000

    Updated implementation of jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,29 @@
 #include "search_algos.h"
 /**
+ * lin_search - searches for a value in an array with linear search
+ *
+ * @array: the array to be searched
+ * @start: the start index to search
+ * @end: the end index to search
+ * @value: the value to search for
+ *
+ * Return: first index where value is found or
+ * -1 if the value is not present
+ */
+int lin_search(int *array, int start, int end, int value)
+{
+	int i;
+
+	for (i = start; i <= end; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value) /* target found */
+			return (i);
+	}
+	return (-1); /* value wasn't found */
+}
+
+/**
  * jump_search - searches for a value in an array with jump search
  *
  * @array: the array to be searched
@@ -11,16 +35,15 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int i;
 	int start_index;
 	int end_index;
 	int step	   = 0;
 	int block_size = (int) sqrt(size);
 
-	if (array == NULL) /* empty array */
+	if (array == NULL || size == 0) /* empty array */
 		return (-1);
 
-	/* Jumping phase -  done in jumps of block_size */
+	/* Jumping phase - done in jumps of block_size */
 	while ((step < (int) size) && (array[step] < value))
 	{
 		printf("Value checked array[%d] = [%d]\n", step, array[step]);
@@ -35,20 +58,8 @@ int jump_search(int *array, size_t size, int value)
 	if (step < (int) size) /* end index for all but last block */
 		end_index = step;
 	else /* end index for last block */
-		end_index = (int) size;
-	printf("Value found between indexes [%d] and [%d]\n", start_index,
-		   step);
-	for (i = start_index; i < end_index; i++)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value) /* target found */
-			return (i);
-	}
-	/* edge case when target is last item in block */
-	if (array[i] == value)
-	{ /* target found */
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		return (i);
-	}
-	return (-1); /* value wasn't found */
+		end_index = (int) size - 1;
+
+	printf("Value found between indexes [%d] and [%d]\n", start_index, step);
+	return (lin_search(array, start_index, end_index, value));
 }
